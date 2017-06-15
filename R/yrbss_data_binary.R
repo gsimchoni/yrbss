7a36ad35426e3eefd8b9088ffa5d7f3392a0e47a
Giora Simchoni <gioras@ibm.com>
2017-06-15 11:35:35 +0300

    Add yrbss_data_binary.R, Change gitignore

---
+++ R/yrbss_data_binary.R
@@ -0,0 +1,21 @@
+#' YRBSS Data - Binary Version
+#'
+#' Data from CDC YRBSS - the Binary Version, where multiple choice questions were grouped into two options.
+#'
+#' @docType data
+#'
+#' @usage data(yrbss_data)
+#'
+#' @format An object of class \code{"tbl_df"};
+#'
+#' @keywords datasets
+#'
+#' @references Centers for Disease Control and Prevention. Youth Risk Behavior Survey Questionnaire. Accessed on [2017-06-14].
+#' (\href{www.cdc.gov/yrbs}{CDC YRBSS Official Site})
+#'
+#' @source \href{www.cdc.gov/yrbs}{CDC YRBSS Official Site}
+#'
+#' @examples
+#' data(yrbss_data_binary)
+#' \donttest{iplotCurves(phe, times)}
+"yrbss_data_binary"
